685dc8779a4bc451a381c512a07e054c2b188b63
weed2021 <quanlbfx12493@funix.edu.vn>
2021-10-04 12:43:49 +0700

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color = "secondary">
         <div className="container">
           <NavbarBrand href="/">Hello</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }

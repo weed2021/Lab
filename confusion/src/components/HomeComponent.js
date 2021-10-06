0c55e78aa3e3fcc968f7cca5b953c6058a40ae4f
weed2021 <quanlbfx12493@funix.edu.vn>
2021-10-06 23:10:35 +0700

    lab05_3

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from "react";
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+
+function RenderCard({item}){
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle><h5>{item.name}</h5></CardTitle>
+                {item.designation ? <CardSubtitle><b>{item.designation}</b></CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props){
     return(
-        <div className="container">
-            <h2>HOME CONTENT</h2>
+        <div className="container mt-3">
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
         </div>
     );
 }
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,10 +3,14 @@ import React,{Component} from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
 import Moment from "react-moment";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {BrowserRouter,Switch,Route,Redirect} from 'react-router-dom';
 
 class App extends Component{
@@ -14,6 +18,9 @@ class App extends Component{
         super(props);
         this.state={
         dishes: DISHES,
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS
         // selectedDish: null
         };
     }
@@ -71,7 +78,10 @@ class App extends Component{
 
         const HomePage=()=>{
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion)=>promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+                />
             );
         }
         return(
@@ -81,8 +91,10 @@ class App extends Component{
                     <Switch>
                         
                         <Route path='/home' component={HomePage}/>
-                        <Route path='/menu' component={()=><Menu dishes={this.state.dishes} />} />
+                        <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes} />} />
+                        <Route exact path='/contact' component={Contact} />
                         <Redirect to='/home' />
+                        
                         
                     </Switch>
                     <Footer />
